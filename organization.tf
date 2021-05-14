dc4f81a2a2e823445fc34883f5fb71020b5e7c58
camus <camus@perxhealth.com>
2021-05-14 16:09:18 +1000

    add security hub

--- organization.tf
+++ organization.tf
@@ -4,6 +4,7 @@ resource "aws_organizations_organization" "org" {
   aws_service_access_principals = [
     "cloudtrail.amazonaws.com",
     "config.amazonaws.com",
+    "securityhub.amazonaws.com"
   ]
 
   enabled_policy_types = ["SERVICE_CONTROL_POLICY"]
